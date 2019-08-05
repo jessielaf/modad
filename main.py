302b55eb61d91daca2b18a5d737f39719cfd72aa
Jessie Liauw A Fong <jessielaff@live.nl>
2019-08-05 22:59:28 +0200

    Add click and dissembler

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
+import click
 import yaml
 from src.assembler import Assembler
+from src.dissembler import Dissembler
 from src.parser import Config
 from src.state import state
 
-if __name__ == "__main__":
-    # todo: make this dynamic with click
-    with open("example_config.yml", "r") as stream:
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument("filename")
+def assemble(filename):
+    """
+    Assembles modular monolith
+    """
+
+    with open(filename, "r") as stream:
         state.config = Config(yaml.safe_load(stream))
         Assembler().run()
+
+
+@cli.command()
+@click.argument("filename")
+@click.argument("module_name")
+@click.argument("dissemble_dest")
+def dissemble(filename, module_name, dissemble_dest):
+    """
+    Dissembles modular monolith
+    """
+    with open(filename, "r") as stream:
+        state.config = Config(yaml.safe_load(stream))
+        Dissembler().run(module_name, dissemble_dest)
+
+
+cli_obj = click.CommandCollection(sources=[cli])
+
+
+if __name__ == "__main__":
+    cli()
---
+++ src/dissembler.py
@@ -0,0 +1,30 @@
+import shutil
+from src.state import state
+from src.utils import clone, remove_dir
+
+
+class Dissembler:
+    dissemble_dest = ""
+
+    def run(self, module_name, dissemble_dest):
+        self.dissemble_dest = dissemble_dest
+
+        module = next(
+            module for module in state.config.modules if module.name == module_name
+        )
+        clone(module, self.dissemble_dest)
+
+        if isinstance(state.config.dest, list):
+            self.multiple_destinations()
+        else:
+            self.single_destination()
+
+    def single_destination(self):
+        shutil.move(state.config.dest, self.dissemble_dest)
+
+    def multiple_destinations(self):
+        for destination in state.config.dest:
+            directory = f"{self.dissemble_dest}/{destination.src}"
+
+            remove_dir(directory)
+            shutil.move(destination.dest, directory)
