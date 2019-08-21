93c7f2c688d11133dadd320fda6fa76ebbe090d6
Jessie Liauw A Fong <jessielaff@live.nl>
2019-08-21 18:59:32 +0200

    Rename hash to commit_hash

--- modad/utils.py
+++ modad/utils.py
@@ -13,19 +13,19 @@ def remove_dir(directory):
     subprocess.check_output(["rm", "-rf", directory])
 
 
-def clone(module, directory, hash=None):
+def clone(module, directory, commit_hash=None):
     """
     Clones a module to a directory
 
     Args:
         module: Module that is defined in the config
         directory: Directory to which it should be copied
-        hash: Hash of the commit that will be checked out. If this is not passed the version of the module will be used
+        commit_hash: Hash of the commit that will be checked out. If this is not passed the version of the module will be used
     """
 
     repo = Repo.clone_from(module.repo, directory)
 
     if hash:
-        repo.git.checkout(hash)
+        repo.git.checkout(commit_hash)
     else:
         repo.git.checkout(module.version)
